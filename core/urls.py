1eb4c2a74708124f79fb1cf4b59bbe31c50cf830
dcalsky <dcalsky@163.com>
2016-10-28 17:04:20 +0800

    common: index html head meta renderer

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<part_name>\w+)$', views.part, name='part'),
-    url(r'^(?P<part_name>\w+)/(?P<article_id>\d+)$', views.article, name='article'),
+    url(r'^part/(?P<part_name>\w+)$', views.part, name='part'),
+    url(r'^part/(?P<part_name>\w+)/(?P<article_id>\d+)$', views.article, name='article'),
     url(r'^appoint$', views.apply_appoint, name='appoint'),
 ]
